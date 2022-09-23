16579e84b281ccc81201cd100b55ecc9842e1feb
Isabela Rozani Floriano <irf161295@gmail.com>
2022-09-23 16:11:55 -0300

    feat: create class Orc from class Race

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -2,7 +2,8 @@ import Race from './Race';
 import Dwarf from './Dwarf';
 import Elf from './Elf';
 import Halfling from './Halfing';
+import Orc from './Orc';
 
-export { Dwarf, Elf, Halfling };
+export { Dwarf, Elf, Halfling, Orc };
 
 export default Race;
