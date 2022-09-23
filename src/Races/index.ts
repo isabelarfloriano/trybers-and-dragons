91ee190b15bf039efbf468e232efc006558d26e5
Isabela Rozani Floriano <irf161295@gmail.com>
2022-09-23 16:09:56 -0300

    feat: create class Halfling from class Race

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,7 +1,8 @@
 import Race from './Race';
 import Dwarf from './Dwarf';
 import Elf from './Elf';
+import Halfling from './Halfing';
 
-export { Dwarf, Elf };
+export { Dwarf, Elf, Halfling };
 
 export default Race;
